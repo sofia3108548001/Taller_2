d2b08b74bad4959a356d0a7464ef890bbdc78f38
Angela sofia Garcia <asgsrciag@udistrital.edu.co>
2024-03-20 15:47:04 -0500

    DADO

---
+++ dado.py
@@ -0,0 +1,11 @@
+import random 
+
+numero= random.randint (2,12)
+
+if numero==7:
+    print("Gano")
+    
+else: 
+    print("deje de jugar")
+
+print (numero)
--- intropython.py
+++ intropython.py
@@ -19,3 +19,48 @@ variable= 'a'
 variable= 3.1415926535
 
 #para asignar un valor a la variable se usa el operador =
+
+
+#OPERADORES: Mecanismo para obtener un dato a partir de otros datos. # los datos qie intervienen se llaman operandos.
+
+#Arimeticos: + - * %
+#De comparacion: Retornan True or False. < >= <= == !=
+#Los de logica booleana: OR AND. Retornan True or False de acuerdo 
+#a una tabla de verdad. Los operadores siempre son booleanos ( True or False)
+
+a=True 
+b=False 
+
+print( a and b)
+
+#los operadores booleanos y de comparacion son muy uyilizamos al definir condiciones 
+
+#Estructura de control de codigo: en general un programa se ejecuta linea por linea de manera secuencial. se puede romper esa secuencialidad empleando un conjunto de secuencias (expresiones) que permite 
+#1. seleccionar la ejecución de un bloque de codigo 
+#2. Repetir la ejecucion de un boque de codigo 
+#3. seleccionar entre ejecutar un bloque de codigo u otro bloque de codigo. 
+# de esta manera ppodemos "romper" la secuencalidad 
+# Principios del paradigma de la programacion 
+
+#1 sentencia if. si se cumple una condicion (se ejecuta un bloque de codig) se ejecuta un bloque de codigo 
+print ("linea 1")
+print("linea 2")
+if 5>8 or 3<7:
+
+    print("esto se muestra si la condicion es verdadera")
+    
+else: 
+    print("Esto muestra si la condicion es false")
+    
+entrada= int(input("cuantos años tiene?"))
+
+if entrada<18:
+    print("eres un menor de edad ")
+    
+else:
+    print("Ya eres mas grande ")
+    
+    
+#Taller" programa em pyton que genere un numero aleatorio ente 1-12 si el numero es 7 gano y si no deje de jugar 
+    
+
